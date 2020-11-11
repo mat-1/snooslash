4720d39f52925a968bca9c566241466ff2130414
mat <27899617+mat-1@users.noreply.github.com>
2020-11-10 21:57:53 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,107 @@
+from moviepy.config import change_settings
+import moviepy.editor as mpy
+from io import BytesIO
+import markdown
+import textwrap
+import requests
+import pyttsx3
+import hashlib
+import re
+
+change_settings({'IMAGEMAGICK_BINARY': r'C:\\Program Files\\ImageMagick-7.0.10-Q16-HDRI\\magick.exe'})
+
+tts = pyttsx3.init()
+
+clips = []
+
+width = 1280
+height = 720
+
+
+def generate_text_clip(text, read_text=None):
+	read_text = read_text or text
+	background_clip = mpy.ColorClip((width, height), color=(26, 26, 27))
+
+	hashed_read_text = hashlib.md5(read_text.encode()).hexdigest()
+	audio_filename = f'temp/{hashed_read_text}.mp3'
+
+	tts.save_to_file(read_text, audio_filename)
+	tts.runAndWait()
+	print('saved to file :)', read_text)
+
+	# text_wrapped = textwrap.fill(text, width // 16 - 2, replace_whitespace=False, drop_whitespace=False)
+
+	text_clip = mpy.TextClip(
+		markdown.matdown_to_pango(text),
+		fontsize=24,
+		font='NotoSans-Regular',
+		color='white',
+		align='east',
+		method='pango',
+		size=(width - 64 * 2, height)
+	).set_position((64, 64))
+
+	audio_clip = mpy.AudioFileClip(audio_filename)
+
+	composite_text = mpy.CompositeVideoClip([background_clip, text_clip])
+	composite_text = composite_text.set_audio(audio_clip)
+	composite_text = composite_text.set_duration(audio_clip.duration)
+	# composite_text = composite_text.set_duration(.1)
+
+	return composite_text
+
+
+def generate_large_text_clip(text):
+	text = markdown.markdown_to_matdown(text)
+	sentences_split = re.findall(r'(.+?)(?:( *[\.\?!][\'"\)\]]*[ \n]*)|$)', text)
+	sentences = [sentence[0] + sentence[1] for sentence in sentences_split]
+	displaying_text = ''
+	clips = []
+	for sentence in sentences[:10]:
+		displaying_text += sentence
+		clips.append(generate_text_clip(displaying_text, sentence))
+	return mpy.concatenate_videoclips(clips)
+
+
+def fetch_post(subreddit=None):
+	if subreddit is None:
+		subreddit = 'nosleep'
+	r = requests.get(f'https://reddit.com/r/{subreddit}.json', headers={
+		'user-agent': 'snooslash'
+	})
+	data = r.json()
+	posts_list = data['data']['children']
+	for post in posts_list:
+		post = post['data']
+		if post['locked']: continue
+		elif post['stickied']: continue
+		elif post['distinguished'] == 'moderator': continue
+		return {
+			'id': post['id'],
+			'title': post['title'],
+			'body': post['selftext']
+		}
+
+
+# md = markdown.markdown_to_matdown('[hello](https://world.com)')
+# print(markdown.matdown_to_pango(md))
+
+# exit()
+
+
+post = fetch_post()
+clips.append(generate_large_text_clip(post['body']))
+
+final_clip = mpy.concatenate_videoclips(clips)
+
+# final_clip = mpy.TextClip(
+# 	'a<b>a</b>',
+# 	fontsize=32,
+# 	font='NotoSans-Regular',
+# 	color='white',
+# 	align='west',
+# 	method='pango'
+# ).set_position((64, 64)).set_duration(1)
+
+
+final_clip.write_videofile('video.avi', fps=30, codec='mpeg4')
---
+++ markdown.py
@@ -0,0 +1,81 @@
+import re
+
+matdown_reset = chr(1000000)
+matdown_href = chr(1000001)
+matdown_monospace = chr(1000002)
+matdown_quote = chr(1000003)
+matdown_bold = chr(1000004)
+matdown_italic = chr(1000005)
+matdown_h1 = chr(1000006)
+matdown_h2 = chr(1000007)
+matdown_h3 = chr(1000008)
+matdown_hr = chr(1000009)
+
+matdown_conversions = {
+	matdown_href: r'<span foreground="#4fbcff">\1</span>',
+	matdown_monospace: r'<code>\1</code>',
+	matdown_quote: r'<blockquote>\1</blockquote>',
+	matdown_bold: r'<b>\1</b>',
+	matdown_italic: r'<i>\1</i>',
+	matdown_h1: r'<span size="2048">\1</span>',
+	matdown_h2: r'<span size="1536">\1</span>',
+	matdown_h3: r'<span size="1198">\1</span>',
+	matdown_hr: r'<span size="1198">\1</span>'
+}
+
+
+# matdown: the best markup language:tm:
+def markdown_to_matdown(content):
+	content = content.replace('\r\n', '\n')
+
+	content = re.sub(
+		r'\[(.{1,}?)\]\(([\w\-.\/:?=#;&]+)\)',
+		matdown_href + r'\1' + matdown_reset,
+		content
+	)  # anchors
+	content = re.sub(
+		r'(?<!["\w])(https?:\/\/[\w\-.]{1,})(?<!["\w])',
+		matdown_href + r'\1' + matdown_reset,
+		content
+	)  # normal link
+	content = re.sub(
+		r'```(\w+|)\n?([\0-\255]+)```',
+		matdown_href + r'\2' + matdown_reset,
+		content
+	)
+	content = re.sub(r'`(.{1,}?)`', matdown_monospace + r'\1' + matdown_reset, content)
+	content = re.sub(r'^&gt; (.{1,}?)\n', matdown_quote + r'\1' + matdown_reset, content, flags=re.MULTILINE)
+	content = re.sub(r'\*\*(.{1,}?)\*\*', matdown_bold + r'\1' + matdown_reset, content)
+	content = re.sub(r'\*(.{1,}?)\*', matdown_italic + r'\1' + matdown_reset, content)
+	content = re.sub(r'^# (.+)\n', matdown_h1 + r'\1' + matdown_reset, content, flags=re.MULTILINE)
+	content = re.sub(r'^## (.+)\n', matdown_h2 + r'\1' + matdown_reset, content, flags=re.MULTILINE)
+	content = re.sub(r'^### (.+)\n', matdown_h3 + r'\1' + matdown_reset, content, flags=re.MULTILINE)
+	content = re.sub(r'^#### (.+)\n', matdown_h3 + r'\1' + matdown_reset, content, flags=re.MULTILINE)
+	content = re.sub(r'^##### (.+)\n', matdown_h3 + r'\1' + matdown_reset, content, flags=re.MULTILINE)
+	content = re.sub(r'^###### (.+)\n', matdown_h3 + r'\1' + matdown_reset, content, flags=re.MULTILINE)
+	content = re.sub(r'\n([-_*])\1{2,}\n', matdown_hr, content)
+
+	return content
+
+
+def matdown_to_pango(content):
+	current_matdown = None
+	output = ''
+	content += matdown_reset
+	for character in content:
+		if (
+			(current_matdown and character == matdown_reset)
+			or (current_matdown and character in matdown_conversions)
+		):
+			if not current_matdown: current_matdown = character
+			pango_open, pango_close = matdown_conversions[current_matdown].split(r'\1')
+			output += pango_close
+			current_matdown = None
+		elif character in matdown_conversions:
+			pango_open, pango_close = matdown_conversions[character].split(r'\1')
+			output += pango_open
+			current_matdown = character
+		elif character != matdown_reset:
+			output += character
+
+	return output

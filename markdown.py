0dfe288a900b7e699d7b6a06a29e7f7f7bc95ae8
mat <27899617+mat-1@users.noreply.github.com>
2020-11-15 21:27:58 -0600

    converted to opencv and fixed bugs

--- markdown.py
+++ markdown.py
@@ -1,4 +1,6 @@
+from PIL import Image, ImageDraw, ImageFont
 import re
+
 
 matdown_reset = chr(1000000)
 matdown_href = chr(1000001)
@@ -26,23 +28,23 @@ matdown_conversions = {
 
 # matdown: the best markup language:tm:
 def markdown_to_matdown(content):
-	content = content.replace('\r\n', '\n')
-
 	content = re.sub(
-		r'\[(.{1,}?)\]\(([\w\-.\/:?=#;&]+)\)',
+		r'\[(.+?)\]\(([\w\-.\/:?=#;&]+)\)',
 		matdown_href + r'\1' + matdown_reset,
 		content
 	)  # anchors
-	content = re.sub(
-		r'(?<!["\w])(https?:\/\/[\w\-.]{1,})(?<!["\w])',
-		matdown_href + r'\1' + matdown_reset,
-		content
-	)  # normal link
-	content = re.sub(
-		r'```(\w+|)\n?([\0-\255]+)```',
-		matdown_href + r'\2' + matdown_reset,
-		content
-	)
+
+	# [Hollow.](https://www.reddit.com/r/nosleep/comments/jrkyr8/im_a_dentist_for_monsters_the_baby_and_the_beast/?utm_source=share&amp;utm_medium=ios_app&amp;utm_name=iossmf)
+	# content = re.sub(
+	# 	r'(?<!["\w])(https?:\/\/[\w\-.]{1,})(?<!["\w])',
+	# 	matdown_href + r'\1' + matdown_reset,
+	# 	content
+	# )  # normal link
+	# content = re.sub(
+	# 	r'```(\w+|)\n?([\0-\255]+)```',
+	# 	matdown_href + r'\2' + matdown_reset,
+	# 	content
+	# )
 	content = re.sub(r'`(.{1,}?)`', matdown_monospace + r'\1' + matdown_reset, content)
 	content = re.sub(r'^&gt; (.{1,}?)\n', matdown_quote + r'\1' + matdown_reset, content, flags=re.MULTILINE)
 	content = re.sub(r'\*\*(.{1,}?)\*\*', matdown_bold + r'\1' + matdown_reset, content)
@@ -58,7 +60,7 @@ def markdown_to_matdown(content):
 	return content
 
 
-def matdown_to_pango(content):
+def matdown_to_markdown(content):
 	current_matdown = None
 	output = ''
 	content += matdown_reset
@@ -91,3 +93,132 @@ def matdown_to_plaintext(content):
 		.replace('&gt;', '>')\
 		.replace('&lt;', '<')
 	return output
+
+
+font_24 = ImageFont.truetype('NotoSans-Regular.ttf', 24)
+
+fonts = {
+	24: font_24
+}
+
+
+def wrap_text(text, line_length, font=None):
+	output = ''
+	current_line_length = 0
+
+	for word in text.split(' '):
+		word = word + ' '
+		word_length = font.getsize(word)[0]
+		new_line_length = current_line_length + word_length
+		if '\n' in word:
+			current_line_length = 0
+		while word_length > line_length:
+			output += word[:30] + '\n'
+			word = word[30:]
+			word_length = font.getsize(word)[0]
+		if new_line_length > line_length:
+			output = output.strip()
+			output += '\n'
+			current_line_length = 0
+		current_line_length += font.getsize(word)[0]
+		output += word.lstrip(' ')
+	return output.strip()
+
+def matdown_to_pages(content, width=1280, height=720):
+	pages = []
+	current_x = 0
+	current_y = 0
+
+	current_effect = None
+
+	text_size = 24
+
+	current_page = ''
+
+	for i, char in enumerate(content):
+		current_font = fonts[text_size]
+		if char == matdown_reset:
+			current_effect = None
+			current_page += char
+		elif char in {
+			matdown_href, matdown_monospace, matdown_quote, matdown_bold, matdown_italic, matdown_h1, matdown_h2, matdown_h3,
+			matdown_hr
+		}:
+			current_effect = char
+			current_page += char
+		elif char == '\n':
+			current_x = 0
+			current_y += text_size * 1.5
+			current_page += char
+		elif char == ' ':
+			next_word = content[i + 1:].split(' ')[0].strip(' .!?')
+			char_width, char_height = current_font.getsize(char)
+			word_width, word_height = current_font.getsize(next_word)
+			if current_x + word_width >= width:
+				current_x = 0
+				current_y += text_size * 1.5
+			else:
+				current_x += char_width
+			current_page += ' '
+		else:
+			char_width, char_height = current_font.getsize(char)
+			if current_x + char_width >= width:
+				current_x = 0
+				current_y += text_size * 1.5
+			current_page += char
+			current_x += char_width
+		if current_y >= height:
+			pages.append(current_page)
+			current_page = ''
+			current_y = 0
+
+	pages.append(current_page)
+	return pages
+
+
+def matdown_to_pillow(content, width=1280, height=720):
+	im = Image.new('RGBA', (width, height))
+
+	d = ImageDraw.Draw(im)
+
+	current_x = 0
+	current_y = 0
+
+	current_effect = None
+
+	text_size = 24
+
+	for i, char in enumerate(content):
+		current_font = fonts[text_size]
+		if char == matdown_reset:
+			current_effect = None
+		elif char in {
+			matdown_href, matdown_monospace, matdown_quote, matdown_bold, matdown_italic, matdown_h1, matdown_h2, matdown_h3,
+			matdown_hr
+		}:
+			current_effect = char
+		elif char == '\n':
+			current_x = 0
+			current_y += text_size * 1.5
+		elif char == ' ':
+			next_word = content[i + 1:].split(' ')[0]
+			char_width, char_height = current_font.getsize(char)
+			word_width, word_height = current_font.getsize(next_word)
+			if current_x + word_width >= width:
+				current_x = 0
+				current_y += text_size * 1.5
+			else:
+				current_x += char_width
+		else:
+			current_color = '#ffffff'
+
+			if current_effect == matdown_href: current_color = '#4fbcff'
+
+			char_width, char_height = current_font.getsize(char)
+			if current_x + char_width >= width:
+				current_x = 0
+				current_y += text_size * 1.5
+			d.text((current_x, current_y), char, font=current_font, fill=current_color)
+			current_x += char_width
+
+	return im
---
+++ video.py
@@ -0,0 +1,179 @@
+from moviepy.audio.AudioClip import CompositeAudioClip, concatenate_audioclips
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.audio.fx.all import audio_normalize
+from moviepy.audio.fx.volumex import volumex
+from mutagen.wave import WAVE
+from PIL import Image
+import thumbnail
+import markdown
+import reddit
+import random
+import numpy
+import cv2
+import tts
+import re
+import os
+
+class RedditVideo:
+	def __init__(self, filename='video.mp4', video_type='post', reddit_data={}, size=(1280, 720)):
+		self.size = size
+		self.filename = filename
+		self.fps = 24
+		self.reddit_data = reddit_data if reddit_data else reddit.fetch_post()
+
+		filename_name_part, filename_ext_part = filename.split('.', 1)
+		self.video_silent_filename = filename_name_part + '-silent.' + filename_ext_part
+		self.video_audio_filename = filename_name_part + '-audio.mp3'
+
+		fourcc = 0x7634706d  # mp4
+		self.v = cv2.VideoWriter(
+			'temp/' + self.video_silent_filename,
+			fourcc,
+			self.fps,
+			self.size
+		)
+
+		self._create()
+
+	def _write_frame(self, im):
+		self._write_frames(im, 1)
+
+	def _write_frames(self, im, times):
+		cv2_frame = cv2.cvtColor(numpy.array(im), cv2.COLOR_RGB2BGR)
+		for _ in range(times):
+			self.v.write(cv2_frame)
+
+	def _get_audio_length(self, filename):
+		audio = WAVE(filename)
+		return audio.info.length
+
+	def _add_video_section(self, type, image, text=None):
+		data = {
+			'type': type,
+			'image': image,
+			'audio': None
+		}
+		duration = 0
+		if text:
+			tts_filename = tts.create_tts_file(text)
+			duration = self._get_audio_length(tts_filename)
+			data['text'] = text
+			data['audio'] = tts_filename
+		data['duration'] = duration
+
+		self.video_sections.append(data)
+
+	def _create_intro(self):
+		thumbnail_im = thumbnail.generate_thumbnail(self.reddit_data)
+		self.thumbnail_im = thumbnail_im
+		self._add_video_section(
+			'image',
+			image=thumbnail_im,
+			text=self.reddit_data['title']
+		)
+
+	def _create_post_part(self, content, new_content):
+		im = Image.new('RGBA', self.size, (26, 26, 27))
+		text_im = markdown.matdown_to_pillow(content, width=self.size[0]-20)
+		im.paste(text_im, (10, 10), text_im)
+		self._add_video_section(
+			'image',
+			image=im,
+			text=markdown.matdown_to_plaintext(new_content)
+		)
+
+	def _create_post_body(self, content):
+		print('content', content)
+		matdown_content = markdown.markdown_to_matdown(content)
+		print('matdown_content', matdown_content)
+		# matdown_content = matdown_content[:300]
+		content_pages = markdown.matdown_to_pages(matdown_content, self.size[0]-20, self.size[1]-20)
+		print(content_pages)
+		for page in content_pages:
+			sentences_split = re.findall(r'(.+?)( *[\.\?!\n][\'"\)\]]*[ \n]*)', page)
+			sentences = [sentence[0] + sentence[1] for sentence in sentences_split]
+			displaying_text_list = []
+
+			for sentence in sentences:
+				print('.', end='')
+				displaying_text_list.append(sentence)
+				displaying_text = ''.join(displaying_text_list)
+				saying_text = markdown.matdown_to_plaintext(sentence)
+				if not re.search('[a-zA-Z]', saying_text): continue
+				self._create_post_part(displaying_text, saying_text)
+
+	def _render(self):
+		for section in self.video_sections:
+			duration_frames = int(section['duration'] * self.fps)
+			image = section['image']
+			self._write_frames(image, duration_frames)
+		
+		self.v.release()
+
+	def _add_audio(self):
+		audio_clips = []
+		t = 0
+		for section in self.video_sections:
+			if section['audio']:
+				audio_clip = AudioFileClip(section['audio'])
+				audio_clip = audio_clip.set_start(t)
+				audio_clips.append(audio_clip)
+			t += section['duration']
+
+		music_clip = self._make_background_music_clip(t)
+		audio_clips.append(music_clip)
+
+		composite_audio_clip = CompositeAudioClip(audio_clips)
+		composite_audio_clip.fps = None
+		composite_audio_clip.write_audiofile('temp/' + self.video_audio_filename)
+
+		for audio_clip in audio_clips:
+			audio_clip.close()
+
+		composite_audio_clip.close()
+
+	def _combine_video_audio(self):
+		video_clip = VideoFileClip('temp/' + self.video_silent_filename)
+		video_clip.write_videofile(self.filename, audio='temp/' + self.video_audio_filename)
+
+	def _clear_temp_files(self):
+		for f in os.listdir('temp'):
+			os.remove(os.path.join('temp', f))
+
+	def _select_song_file(self):
+		songs = os.listdir('music')
+		song_filename = random.choice(songs)
+		print(song_filename)
+		return 'music/' + song_filename
+
+
+	def _make_background_music_clip(self, length):
+		music_clips = []
+		current_length = 0
+
+		while length > current_length:
+			song_filename = self._select_song_file()
+			music_clip = AudioFileClip(song_filename)
+			music_clips.append(music_clip.fx(audio_normalize).fx(volumex, 0.025))
+
+			current_length = concatenate_audioclips(music_clips).duration
+
+		return concatenate_audioclips(music_clips).set_end(length)
+
+
+	def _create(self):
+		print('Starting...')
+		self.video_sections = []
+		self._create_intro()
+		self._create_post_body(self.reddit_data['body'])
+
+		print('\nPrepared how the video should look, now writing frames using Pillow.')
+
+		self._render()
+
+		print('\nFinished writing frames! Now adding audio.')
+		self._add_audio()
+		self._combine_video_audio()
+		self._clear_temp_files()
+		print('\nFinished!')

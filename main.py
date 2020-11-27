f8bf5f882eb0f1483a3e30701a2ebfc2ee497e31
mat <27899617+mat-1@users.noreply.github.com>
2020-11-27 14:40:40 -0600

    added reddit id extraction from url to main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import video
 
-v = video.RedditVideo(post_id='jxxrzg')
+def extract_id(url):
+	url_parts = url.strip('/').split('/')
+	comments_index = url_parts.index('comments')
+	if comments_index:
+		return url_parts[comments_index + 1]
+	else:
+		# the input was probably an id anyway
+		return url_parts[-1]
+
+
+post_id = extract_id('https://www.reddit.com/comments/k211no')
+
+v = video.RedditVideo(post_id=post_id)

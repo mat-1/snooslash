4889756be063d32908f65382d7f24f627b79dcff
mat <27899617+mat-1@users.noreply.github.com>
2020-11-27 16:12:28 -0600

    Add input for post

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ import video
 
 def extract_id(url):
 	url_parts = url.strip('/').split('/')
-	comments_index = url_parts.index('comments')
-	if comments_index:
+	if 'comments' in url_parts:
+		comments_index = url_parts.index('comments')
 		return url_parts[comments_index + 1]
 	else:
 		# the input was probably an id anyway
 		return url_parts[-1]
 
+subreddits = [
+	'nosleep',
+	'entitledparents',
+	'tifu',
+	'maliciouscompliance',
+	'talesfromtechsupport',
+	'TalesFromRetail'
+]
 
-post_id = extract_id('https://www.reddit.com/comments/k211no')
+print('Recommended subreddits to look at: ')
+for subreddit in subreddits:
+	print(f'- r/{subreddit}')
+post_id = extract_id(
+	input('Enter Reddit post url or id > ')
+)
 
 v = video.RedditVideo(post_id=post_id)

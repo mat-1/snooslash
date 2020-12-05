8a48b36d0d478c1db3d63de247a834314c941dd5
mat <27899617+mat-1@users.noreply.github.com>
2020-12-05 15:54:33 -0600

    add auto scheduling

--- upload.py
+++ upload.py
@@ -1,15 +1,54 @@
 from simple_youtube_api.LocalVideo import LocalVideo
 from simple_youtube_api.Channel import Channel
+from datetime import datetime, timedelta
 import json
+
+
+def get_upload_time():
+	epoch = datetime(1970, 1, 1)
+	now = datetime.utcnow()
+	with open('publish_day.txt', 'r') as f:
+		publish_day = f.read()
+
+	if not publish_day:
+		publish_day = (now - epoch).days + 1
+	else:
+		publish_day = int(publish_day)
+		if publish_day < (now - epoch).days + 1:
+			publish_day = (now - epoch).days + 1
+	publish_datetime = datetime.fromtimestamp(publish_day * 86400)
+	publish_datetime -= timedelta(hours=6) # upload at midday cst
+	strftime = publish_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
+	return strftime
+
+def increase_upload_time():
+	epoch = datetime(1970, 1, 1)
+	now = datetime.utcnow()
+	with open('publish_day.txt', 'r') as f:
+		publish_day = f.read()
+
+	if not publish_day:
+		publish_day = (now - epoch).days + 1
+	else:
+		publish_day = int(publish_day)
+		if publish_day < (now - epoch).days + 1:
+			publish_day = (now - epoch).days + 1
+	publish_day += 1
+	with open('publish_day.txt', 'w') as f:
+		f.write(str(publish_day))
+
+	
 
 # loggin into the channel
 channel = Channel()
 channel.login('client_secret.json', 'credentials.storage')
 
 # setting up the video that is going to be uploaded
-video = LocalVideo(file_path="video.mp4")
+video = LocalVideo(file_path='video.mp4')
 
 # setting snippet
+
+tags = ['reddit', 'rslash', 'story', 'storytime', 'nosleep', 'r/', 'snoo/', 'snooslash', 'reddit storytime', 'entitledparents', 'tifu', 'maliciouscompliance', 'reddit funny', 'top posts', 'comedy', 'best of reddit', 'funny', 'top posts of r/', 'reddit stories', 'askreddit', 'toadfilms', 'giofilms', '\\r', '/r', 'sub', 'subreddit', 'stories', 'memes', 'reddit story', 'cowbelly', 'updoot', 'sir reddit']
 
 with open('post.json', 'r') as f:
 	post_data = json.loads(f.read())
@@ -22,9 +61,11 @@ print('Original Reddit title:', post_data['title'])
 title = input('Enter YouTube post title: ')
 video.set_title(title)
 video.set_description(description)
-video.set_tags(['snooslash', 'reddit', 'reddit r/'])
-video.set_category('gaming')
+video.set_tags(tags)
+video.set_category('people')
 video.set_default_language('en-US')
+
+video.set_publish_at(get_upload_time())
 
 # setting status
 video.set_embeddable(True)
@@ -41,3 +82,5 @@ print(video)
 
 # liking video
 video.like()
+
+increase_upload_time()
--- video.bat
+++ video.bat
@@ -1,3 +1,4 @@
 @echo off
 python main.py
+python upload.py
 pause
